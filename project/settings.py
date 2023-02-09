3dd672a4e151e99d1b15bdafd5cc6b4fe90da3f5
Torgbui-Hiram <hiramkumado@gmail.com>
2023-02-09 21:15:29 +0000

    small changes

--- project/settings.py
+++ project/settings.py
@@ -10,12 +10,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'YOUR_SECRET_KEY'
+SECRET_KEY = '-$h__8$9&xns3s+f#^2y()ue$=5)f2q9d+8m#pk33grea@kqkd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -47,7 +47,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'YOUR_EMAIL'
+EMAIL_HOST_USER = 'hiramcode@gmail.com'
 EMAIL_HOST_PASSWORD = 'PASSWORD'
 
 
@@ -149,8 +149,8 @@ USE_TZ = True
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-STATIC_ROOT = Path(BASE_DIR / 'staticfiles')
-STATIC_URL = 'static/'
+STATIC_ROOT = Path(BASE_DIR / 'static')
+STATIC_URL = '/static/'
 
 MEDIA_ROOT = Path(BASE_DIR / 'media')
 MEDIA_URL = '/media/'
